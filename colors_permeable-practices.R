4d34ea8e53c83d010ead9bf9a7d9d2a1539ff7bd
bbest <ben@ecoquants.com>
2018-10-21 22:58:39 -0700

    perty icons

--- colors_permeable-practices.R
+++ colors_permeable-practices.R
@@ -1,8 +1,11 @@
 library(RColorBrewer)
-display.brewer.all()
+#display.brewer.all()
 
 pal <- colorRampPalette(brewer.pal(11, "RdYlGn"))
-paste(pal(14), collapse=", ")
+paste(pal(6), collapse=", ")
+# #A50026, #F46D43, #FEE08B, #D9EF8B, #66BD63, #006837
+
+# paste(pal(14), collapse=", ")
 # #A50026, #CB2426, #E65036, #F6814C, #FDB164, #FDD884, #FEF3AB, #F0F8AA, #D1EB85, #A9DA6C, #79C565, #42AB5A, #138C4A, #006837
 
-filled.contour(volcano, color.palette = pal, asp = 1)
+#filled.contour(volcano, color.palette = pal, asp = 1)
